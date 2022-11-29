dbf1ad63616e0e069ed4fa107cf987b667a7b270
Yo Yehudi <yochannah@gmail.com>
2022-11-29 20:29:56 +0000

    Update render.js

--- render.js
+++ render.js
@@ -1,5 +1,5 @@
 function renderHello() {
   var template = document.getElementById('template').innerHTML;
-  var rendered = Mustache.render(template, { name: 'Luke' });
+  var rendered = Mustache.render(template, cats);
   document.getElementById('target').innerHTML = rendered;
 }
